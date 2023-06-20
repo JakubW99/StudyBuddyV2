4aaa4e1fdb2633e31da559b508c884cb2eeb6094
JakubW99 <92157165+JakubW99@users.noreply.github.com>
2023-06-20 14:54:44 +0200

    added user controller

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using API.Dto;
+using Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly StudyBuddyDbContext _context;
+        public UserController(StudyBuddyDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IQueryable<UserDto> GetUsers()
+        {
+            var users = _context.Users.Where(x => x.UserName != null).Select(u => new UserDto
+            {
+                Id = u.Id,
+                UserName = u.UserName,
+                Email = u.Email
+            });
+            if (users != null)
+            {
+                return users;
+            }
+            else return null;
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public UserDto GetUserById([FromRoute]  int id) 
+        {
+           var user =  _context.Users.FirstOrDefault(u => u.Id == id);
+                return new UserDto(user.Id, user.UserName, user.Email);
+        }
+    }
+    
+}
---
+++ API/Dto/UserDto.cs
@@ -0,0 +1,19 @@
+﻿namespace API.Dto
+{
+    public class UserDto
+    {
+        public int Id { get; set; } 
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public UserDto()
+        {
+
+        }
+        public UserDto(int id, string userName, string email)
+        {
+            Id = id;
+            UserName = userName;
+            Email = email;
+        }
+    }
+}
